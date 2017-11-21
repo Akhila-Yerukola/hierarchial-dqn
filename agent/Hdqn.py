0c9b316cd4d210ae71532dfb0f17040ffe9fae18
Megha Jhunjhunwala <jjw.megha@gmail.com>
2017-11-21 12:38:36 -0800

    HDQN

--- agent/Hdqn.py
+++ agent/Hdqn.py
@@ -74,17 +74,21 @@ class Hdqn:
 		vector = np.concatenate([state, goal], axis=1)
 		#print "vector"
 		if random.random() < self.actor_epsilon[goal_value-1]:
+			print "Exploring action"
+			return torch.IntTensor([random.randrange(2)])
 			#print "Here ------>", self.actor(Variable(torch.from_numpy(vector).float())).data.numpy()
-			return self.actor(Variable(torch.from_numpy(vector).type(torch.FloatTensor), volatile=True)).data.max(1)[1]
-		return torch.IntTensor([random.randrange(2)])
+		return self.actor(Variable(torch.from_numpy(vector).type(torch.FloatTensor), volatile=True)).data.max(1)[1]
+		
 
 	def select_goal(self, state):
-		if self.meta_epsilon < random.random():
-			pred = self.meta_controller(Variable(torch.from_numpy(state).type(torch.FloatTensor), volatile=True))
-			#print("pred shape: " + str(pred.shape))
-			return pred.data.max(1)[1]
-		print("Exploring");
-		return torch.IntTensor([random.randrange(6)])
+		if random.random() < self.meta_epsilon:
+			print("Exploring goal")
+			return torch.IntTensor([random.randrange(6)])
+
+		pred = self.meta_controller(Variable(torch.from_numpy(state).type(torch.FloatTensor), volatile=True))
+		return pred.data.max(1)[1]
+		
+		
 
 	def criticize(self, goal, next_state):
 		return 1.0 if goal == next_state else 0.0
@@ -113,7 +117,7 @@ class Hdqn:
 		reward_batch = np.array([exp.reward for exp in exps])
 		reward_batch_var = Variable(torch.from_numpy(reward_batch).type(torch.FloatTensor))
 
-		done_batch = np.array([exp.reward for exp in exps])
+		done_batch = np.array([exp.done for exp in exps])
 		not_done_batch_mask = Variable(torch.from_numpy(1- done_batch).type(torch.FloatTensor))
 		#print "state_vectors", state_vectors
 		next_state_vectors = np.squeeze(np.asarray([np.concatenate([exp.next_state, exp.goal], axis=1) for exp in exps]))
@@ -131,58 +135,15 @@ class Hdqn:
 			next_state_vectors = np.expand_dims(next_state_vectors, axis=0)
 			next_state_max_reward = self.target_actor(next_state_vectors_var).detach().max(1)[0]
 		
-		'''
-		new_reward_vectors = deepcopy(reward_vectors.data.numpy())
-		#print new_reward_vectors, reward_vectors.data.numpy(), "new and old reward_vectors"
-		#print "new_rew", new_reward_vectors
-		for i, exp in enumerate(exps):
-			#print exp
-			new_reward_vectors[i][exp.action] = exp.reward
-			#print "val", new_reward_vectors[i][exp.action], exp.reward
-			if not exp.done:
-				new_reward_vectors[i][exp.action] = new_reward_vectors[i][exp.action] + self.gamma * max(next_state_reward_vectors[i])
-				#print "val", new_reward_vectors[i][exp.action], self.gamma * max(next_state_reward_vectors[i])
-		#reward_vectors = np.asarray(reward_vectors)
-		'''
 		next_state_reward_vectors = not_done_batch_mask * next_state_max_reward
 		target_Q_values = reward_batch_var + ( self.gamma * next_state_reward_vectors)
-
 		loss = F.smooth_l1_loss(reward_vectors, target_Q_values)
-
-		#print"state_dict", self.actor.state_dict()
-		#print "new reward_vectors", torch.from_numpy(new_reward_vectors)
 		self.target_actor.load_state_dict(self.actor.state_dict())
-
-		
-		'''
-		actor_weights = self.actor.parameters()
-		actor_target_weights = self.target_actor.parameters()
-		
-		for actor_target_weight, actor_weight in zip(actor_target_weights, actor_weights):
-			actor_target_weight.data = self.target_tau * actor_weight.data + (1 - self.target_tau) * actor_target_weight.data
-		
-		'''
-		
-		
-
-
-		#optimizer = optim.RMSprop(self.actor.parameters(), lr=0.001, alpha=0.9, eps=1e-06)
-
 		self.actor_optimizer.zero_grad()
-
-		
-		#print new_reward_vectors, "new"
-		#print reward_vectors.data.numpy(), "num"
-		
-		#print "loss", loss
 		loss.backward()
 		for param in self.actor.parameters():
 			param.grad.data.clamp_(-1, 1)
 		self.actor_optimizer.step()
-
-		#self.actor.fit(state_vectors, reward_vectors, verbose=0)
-		
-		#Update target network
 		
 
 	def _update_meta(self):
@@ -200,7 +161,7 @@ class Hdqn:
 		reward_batch = np.array([exp.reward for exp in exps])
 		reward_batch_var = Variable(torch.from_numpy(reward_batch).type(torch.FloatTensor))
 
-		done_batch = np.array([exp.reward for exp in exps])
+		done_batch = np.array([exp.done for exp in exps])
 		not_done_batch_mask = Variable(torch.from_numpy(1- done_batch).type(torch.FloatTensor))
 		#print "state_vectors", state_vectors
 		next_state_vectors = np.concatenate([exp.next_state for exp in exps])
@@ -217,57 +178,17 @@ class Hdqn:
 		except Exception as e:
 			next_state_vectors = np.expand_dims(next_state_vectors, axis=0)
 			next_state_max_reward = self.target_meta_controller(next_state_vectors_var).detach().max(1)[0]
-		
-		'''
-		new_reward_vectors = deepcopy(reward_vectors.data.numpy())
-		#print new_reward_vectors, reward_vectors.data.numpy(), "new and old reward_vectors"
-		#print "new_rew", new_reward_vectors
-		for i, exp in enumerate(exps):
-			#print exp
-			new_reward_vectors[i][exp.action] = exp.reward
-			#print "val", new_reward_vectors[i][exp.action], exp.reward
-			if not exp.done:
-				new_reward_vectors[i][exp.action] = new_reward_vectors[i][exp.action] + self.gamma * max(next_state_reward_vectors[i])
-				#print "val", new_reward_vectors[i][exp.action], self.gamma * max(next_state_reward_vectors[i])
-		#reward_vectors = np.asarray(reward_vectors)
-		'''
+
 		next_state_reward_vectors = not_done_batch_mask * next_state_max_reward
 		target_Q_values = reward_batch_var + ( self.gamma * next_state_reward_vectors)
 
 		loss = F.smooth_l1_loss(reward_vectors, target_Q_values)
-
-		#print"state_dict", self.actor.state_dict()
-		#print "new reward_vectors", torch.from_numpy(new_reward_vectors)
 		self.target_meta_controller.load_state_dict(self.meta_controller.state_dict())
-
-		
-		'''
-		actor_weights = self.actor.parameters()
-		actor_target_weights = self.target_actor.parameters()
-		
-		for actor_target_weight, actor_weight in zip(actor_target_weights, actor_weights):
-			actor_target_weight.data = self.target_tau * actor_weight.data + (1 - self.target_tau) * actor_target_weight.data
-		
-		'''
-		
-		
-		#optimizer = optim.RMSprop(self.actor.parameters(), lr=0.001, alpha=0.9, eps=1e-06)
-
 		self.meta_optimiser.zero_grad()
-
-		
-		#print new_reward_vectors, "new"
-		#print reward_vectors.data.numpy(), "num"
-		
-		#print "loss", loss
 		loss.backward()
 		for param in self.meta_controller.parameters():
 			param.grad.data.clamp_(-1, 1)
 		self.meta_optimiser.step()
-
-		#self.actor.fit(state_vectors, reward_vectors, verbose=0)
-		
-		#Update target network
 
 	def update(self, meta=False):
 			if meta:
